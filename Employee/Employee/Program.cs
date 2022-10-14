cd76e6264f96b4cab91076d9a09f543b705138f3
Rajesh kumar <rajeshbabu656656.com>
2022-10-14 15:48:30 +0530

    Add PArt time employee & wage

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -1,27 +1,25 @@
-﻿namespace EmployeeWage
+﻿internal class Program
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    static void Main(string[] args) {
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int EMP_RATE_PER_HOUR = 20;
+        // Variables
+        int empHrs = 0;
+        int empWage = 0;
+        Random random = new Random();
+        int empCheck = random.Next(0, 3);
+        if (empCheck == IS_PART_TIME)
         {
-            // Constants
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
-            // Variables
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
-            }
+            empHrs = 4;
+        }
+        else if (empCheck == IS_FULL_TIME)
+        {
+            empHrs = 8;
+        }
+        else
+        {
+            empHrs = 0;
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
         }
-    }
-}
