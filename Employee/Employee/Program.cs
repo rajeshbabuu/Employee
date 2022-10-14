0e70ef780f6750419a1ff97dbd035308d8486be9
Rajesh kumar <rajeshbabu656656.com>
2022-10-14 17:08:02 +0530

    Ability to compute Employee Wage for multiple companies

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -1,20 +1,21 @@
-﻿ namespace EmployeeWage
+﻿namespace EmployeeWage
 {
     internal class Program
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-        static void Main(string[] args)
+        //public const int EMP_RATE_PER_HOUR = 20;
+        //public const int NUM_OF_WORKING_DAYS = 2;
+        //public const int MAX_HRS_IN_MONTH = 10;
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
             // Variables
             int empHrs = 0;
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
             // Computation
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -31,9 +32,17 @@
                         empHrs = 0;
                         break;
                 }
-                int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-                Console.WriteLine("Toatal Employee Wage : " + totalEmpWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day# : " + totalWorkingDays + "Emp Hrs : " + empHrs);
             }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Toatal Employee Wage for company : " + company + "is" + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage("DMART", 20, 2, 10);
+            computeEmpWage("Reliance", 10, 4, 20);
         }
     }
 }
