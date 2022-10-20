531f423de9550746a193c4d59f51f60bd041cc2f
Rajesh kumar <rajeshbabu656656.com>
2022-10-20 22:35:39 +0530

    employee UC10

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -1,17 +1,18 @@
-﻿using System;
-
-namespace Employee
+﻿namespace Employee
 {
-    internal class Program
-    { 
-        static void Main(string[] args)
+
+
+    class Program
     {
-        EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
-        EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
-        dMart.computeEmpWage();
-        Console.WriteLine(dMart.toString());
-        reliance.computeEmpWage();
-        Console.WriteLine(reliance.toString());
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilderArray = new EmpWageBuilderArray();
+            empWageBuilderArray.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilderArray.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilderArray.computeEmpWage();
+        }
     }
 }
-}
+
+    
+
